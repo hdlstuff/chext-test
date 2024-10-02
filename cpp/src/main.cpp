e216129023c49c8a5ec6170073736d406c938ec3
Canberk Sönmez <canberk.sonmez.409@gmail.com>
2024-10-02 18:09:38 +0200

    Trace + modify the HDL a bit.

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -7,6 +7,7 @@ using namespace sc_core;
 using namespace sc_dt;
 
 #include <VElasticModule.h>
+#include <verilated_vcd_sc.h>
 
 #include <sysc/kernel/sc_spawn.h>
 constexpr struct {
@@ -31,22 +32,22 @@ struct ElasticModule : sc_module {
         , source1 { "source1", clock, reset, source1_data }
         , source2 { "source2", clock, reset, source2_data }
         , sink { "sink", clock, reset, sink_data }
-        , verilatedModule_("dut") {
+        , verilatedModule("dut") {
 
-        verilatedModule_.clock(clock);
-        verilatedModule_.reset(reset);
+        verilatedModule.clock(clock);
+        verilatedModule.reset(reset);
 
-        verilatedModule_.source1_bits(source1_data);
-        verilatedModule_.source1_ready(source1.ready);
-        verilatedModule_.source1_valid(source1.valid);
+        verilatedModule.source1_bits(source1_data);
+        verilatedModule.source1_ready(source1.ready);
+        verilatedModule.source1_valid(source1.valid);
 
-        verilatedModule_.source2_bits(source2_data);
-        verilatedModule_.source2_ready(source2.ready);
-        verilatedModule_.source2_valid(source2.valid);
+        verilatedModule.source2_bits(source2_data);
+        verilatedModule.source2_ready(source2.ready);
+        verilatedModule.source2_valid(source2.valid);
 
-        verilatedModule_.sink_bits(sink_data);
-        verilatedModule_.sink_ready(sink.ready);
-        verilatedModule_.sink_valid(sink.valid);
+        verilatedModule.sink_bits(sink_data);
+        verilatedModule.sink_ready(sink.ready);
+        verilatedModule.sink_valid(sink.valid);
     }
 
 public:
@@ -59,7 +60,8 @@ private:
     DataSignal<uint32_t> source2_data;
     DataSignal<uint32_t> sink_data;
 
-    VElasticModule verilatedModule_;
+public:
+    VElasticModule verilatedModule;
 };
 
 struct Testbench : sc_module {
@@ -67,19 +69,19 @@ struct Testbench : sc_module {
 
     Testbench(sc_module_name const& name = "Testbench")
         : sc_module { name }
-        , elasticModule_ { "elasticModule" }
+        , dut { "elasticModule" }
         , clock_("clock")
         , reset_("reset") {
 
-        elasticModule_.reset(reset_);
-        elasticModule_.clock(clock_);
+        dut.reset(reset_);
+        dut.clock(clock_);
 
         SC_THREAD(thread0);
     }
 
-private:
-    ElasticModule elasticModule_;
+    ElasticModule dut;
 
+private:
     sc_clock clock_;
     sc_signal<bool> reset_;
 
@@ -98,26 +100,38 @@ private:
             wait(20, SC_NS);
 
             for (int i = 0; i < 50; ++i)
-                elasticModule_.source1.send<uint32_t>(i * 10 + 3);
+                dut.source1.send<uint32_t>(i * 10 + 3);
         };
 
         SC_SPAWN {
             wait(20, SC_NS);
             for (int i = 0; i < 50; ++i)
-                elasticModule_.source2.send<uint32_t>(i * 20 + 3);
+                dut.source2.send<uint32_t>(i * 20 + 3);
         };
 
         SC_SPAWN {
             wait(20, SC_NS);
             for (int i = 0; i < 50; ++i)
-                std::cout << "received: " << elasticModule_.sink.receive<uint32_t>() << std::endl;
+                std::cout << "received: " << dut.sink.receive<uint32_t>() << std::endl;
         };
     }
 };
 
 int sc_main(int argc, char** argv) {
+    Verilated::commandArgs(argc, argv);
+    Verilated::traceEverOn(true);
+
     Testbench testbench;
+
+    sc_start(SC_ZERO_TIME);
+
+    std::unique_ptr<VerilatedVcdSc> trace_file = std::make_unique<VerilatedVcdSc>();
+    testbench.dut.verilatedModule.trace(trace_file.get(), 99);
+    trace_file->open("ElasticModule.vcd");
+
     sc_start(50, SC_NS);
+
+    trace_file->close();
 
     return 0;
 }
